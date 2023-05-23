c5314d87d15b5159dbb8a11a777023f9e189ab4c
bartek33642 <92989898+bartek33642@users.noreply.github.com>
2023-05-23 21:10:57 +0200

    lab11

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { BlogItemComponent } from './components/blog-item/blog-item.component';
 import { BlogItemTextComponent } from './components/blog-item-text/blog-item-text.component';
 import { BlogItemImageComponent } from './components/blog-item-image/blog-item-image.component';
 import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { DataService } from "./services/data.service";
 import { SummaryPipe } from './pipes/summary.pipe';
 import { SearchBarComponent } from './shared/search-bar/search-bar.component';
@@ -16,6 +16,13 @@ import { FormsModule } from '@angular/forms';
 import { BlogHomeComponent } from './components/blog-home/blog-home.component';
 import { FilterTextPipe } from './pipes/filter-text.pipe';
 import { TextFormatDirective } from './directives/text-format.directive';
+import { HomeComponent } from './components/home/home.component';
+import { AuthService } from './services/auth.service';
+import {AuthInterceptor} from './services/auth/auth.interceptor';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
 
 
 @NgModule({
@@ -31,7 +38,11 @@ import { TextFormatDirective } from './directives/text-format.directive';
     BlogHomeComponent,
     FilterTextPipe,
     TextFormatDirective,
-    TextFormatDirective
+    TextFormatDirective,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +52,14 @@ import { TextFormatDirective } from './directives/text-format.directive';
 
   ],
   providers: [
-    DataService
+    DataService,
+    AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  
   ],
   bootstrap: [AppComponent]
 })
